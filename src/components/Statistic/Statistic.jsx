01528a22458461fbdc4b4df65ef790250d70cb9e
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-07-02 11:00:10 +0300

    add

--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -30,7 +30,16 @@ export class Statistic extends Component {
 
   render() {
     return (
-      <div>
+      <div
+        style={{
+          height: '100vh',
+          display: 'block',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
         <StatisticTitle title="Please leave feedback" />
 
         <StatisticButton
